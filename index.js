343b9e71d1a2821b8bb1297590e9f4a4497e9b13
K H <kh512@my.yorku.ca>
2025-02-15 21:15:00 -0500

    js for clock + moved script tag to before end body tag

--- index.js
+++ index.js
@@ -7,3 +7,37 @@ function openMenu() {
     }
 }
 
+let button = document.getElementById('addtask');
+if (button) {
+    button.addEventListener('click', addTask);
+}
+
+let taskInput = document.getElementById('task');
+if (taskInput){
+    taskInput.addEventListener('keypress', function(e){
+        if (e.key === 'Enter') {
+            addTask();
+        }
+    });
+}
+
+function addTask() {
+    console.log('addTask trigger');
+    let task = document.getElementById('task');
+    console.log(task.value);
+
+    let taskVal = task.value;
+    if (taskVal === '') {
+        alert('Task cannot be empty');
+        return;
+    }
+
+    let newTask = document.createElement('li');
+    newTask.appendChild(document.createTextNode(taskVal));
+
+    let taskList = document.getElementById('taskList');
+    console.log(newTask);
+    taskList.appendChild(newTask);
+    task.value = '';
+}
+

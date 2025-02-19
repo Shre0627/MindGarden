04572a2c5a686bca7234aa9322db829285e79cfd
Sarah <SarahAlkhalidy331@gmail.com>
2025-02-19 18:42:15 -0500

    made menu and made to do and achivment buttons work

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
 function openMenu() {
    let nav = document.getElementsByClassName("green-box")[0];
-   if(nav.style.display === "none"|| nav.style.display === "") {
-      nav.style.display = "block";
-   } else {
-      nav.style.display = "none";
+    if(nav.style.display === "block" || nav.style.display === "") {
+        nav.style.display = "none";
+    } else {
+        nav.style.display = "block";
+    }
    }
-}
+   function openToDo(){
+   let list = document.getElementsByClassName("box-list")[0];
+   if(list.style.display === "block" || list.style.display === "") {
+       list.style.display = "none";
+   } else {
+       list.style.display = "block";
 
+   }
+
+
+   }
+   function OpenAch(){
+  let ach = document.getElementsByClassName("box-ach")[0];
+  if(ach.style.display === "block" || ach.style.display === "") {
+      ach.style.display = "none";       
+  } else {
+        ach.style.display = "block";
+  }
+  
+   }
